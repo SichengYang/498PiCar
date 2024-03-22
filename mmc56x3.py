e9eb29a8c68f8f3757b6141d0ade28c40c1668ee
Mingkang You <123138506+MingkangYou@users.noreply.github.com>
2024-03-22 15:24:03 -0500

    Update mmc56x3.py

--- mmc56x3.py
+++ mmc56x3.py
@@ -1,8 +1,7 @@
 import board
 import adafruit_mmc56x3
 i2c = board.I2C() # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C() # For using the built-in STEMMA QT connector on a
-microcontroller
+# i2c = board.STEMMA_I2C() # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_mmc56x3.MMC5603(i2c)
 while True:
   mag_x, mag_y, mag_z = sensor.magnetic
